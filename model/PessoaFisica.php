5e30d40ccb22568d26c83b10d0a1775803866430
Edenilson Dias <ededias4@gmail.com>
2020-04-04 12:41:23 -0300

    refatoracao

--- model/PessoaFisica.php
+++ model/PessoaFisica.php
@@ -11,7 +11,8 @@
  *
  * @author Thyago
  */
-class PessoaFisica {
+class PessoaFisica
+{
     //put your code here
 
     private $nome;
@@ -19,7 +20,7 @@ class PessoaFisica {
     private $telefone;
     private $sexo;
     private $datadenascimento;
-    
+
     private $cep;
     private $logradouro;
     private $numero;
@@ -29,92 +30,14 @@ class PessoaFisica {
     private $estado;
     private $categoria;
 
-    // inicio getters
 
-    function getNome() {
-        return $this->nome;
+    function __get($atributo)
+    {
+        return $this->$atributo;
     }
-
-    function getEmail() {
-        return $this->email;
-    }
-    function getTelefone() {
-        return $this->telefone;
-    }
-    function getSexo() {
-        return $this->sexo;
-    }
-    function getDatadenascimento() {
-        return $this->datadenascimento;
-    }
-    function getCep() {
-        return $this->cep;
-    }
-    function getLogradouro() {
-        return $this->logradouro;
-    }
-    function getNumero() {
-        return $this->numero;
-    }
-    function getComplemento() {
-        return $this->complemento;
-    }
-    function getBairro() {
-        return $this->bairro;
-    }
-    function getCidade() {
-        return $this->cidade;
-    }
-    function getEstado() {
-        return $this->estado;
-    }
-    function getCategoria() {
-        return $this->categoria;
-    }
-    // fim getter
-    
-    // inicio setters
-
-    function setNome($nome) {
-        $this->nome = $nome;
-    }
-    function setemail($email) {
-        $this->email = $email;
+    function __set($valor, $atributo)
+    {
+        $this->$valor = $atributo;
     }
 
-    function setTelefone($telefone) {
-        $this->telefone = $telefone;
-    }
-    function setSexo($sexo) {
-        $this->sexo = $sexo;
-    }
-    function setDatanascimento($datanascimento) {
-        $this->datanascimento = $datanascimento;
-    }
-    function setCep($cep) {
-        $this->cep = $cep;
-    }
-    function setLogradouro($logradouro) {
-        $this->logradouro = $logradouro;
-    }
-    function setNumero($numero) {
-        $this->numero = $numero;
-    }
-    function setCompletomento($completomento) {
-        $this->completomento = $completomento;
-    }
-    function setBairro($bairro) {
-        $this->bairro = $bairro;
-    }
-    function setCidade($cidade) {
-        $this->cidade = $cidade;
-    }
-
-    function setEstado($estado) {
-        $this->estado = $estado;
-    }
-    function setCategoria($categoria) {
-        $this->categoria = $categoria;
-    }
-    
 }
