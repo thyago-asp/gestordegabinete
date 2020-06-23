4414dd785a281a130800d82d20fe60e84b932f4f
Edenilson Dias <ededias4@gmail.com>
2020-06-23 20:54:32 -0300

    correcao salvar arquivos

--- estrutura/requerimentos.php
+++ estrutura/requerimentos.php
@@ -109,12 +109,17 @@ if ($url == "declaracoes") {
                     </select>
                 </div>
                 <label>Arquivos</label>
-                <div class="form-group">
-                    <div class="form-line">
-                        <input type="file" class="form-control" name="arquivos[]" multiple id="arquivos">
-
+                <div class="input-group mb-3">
+                    <div class="custom-file" lang="pt">
+                        <input type="file"name="arquivos[]" placeholder="teste" multiple id="arquivos" class="custom-file-input">
+                        
+                        <label  class="custom-file-label" for="arquivos"  id="nomeArq"aria-describedby="inputGroupFileAddon02">Selecione um arquivo</label>
                     </div>
+                    <!-- <div class="input-group-append">
+                        <span class="input-group-text" id="inputGroupFileAddon02">Selecionar</span>
+                    </div> -->
                 </div>
+                
                 <input type="hidden" name="tipo" value="<?php echo $input ?>">
                 <div class="form-group">
                     <button type="submit" class="btn btn-success w-100">Cadastrar</button>
